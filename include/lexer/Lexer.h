23ad00f5b86a5b0057090659cec911cca90a6bf9
hirwa <hirwanshutiflx@gmail.com>
2025-01-27 06:29:24 +0530

    added linter rules

--- include/lexer/Lexer.h
+++ include/lexer/Lexer.h
@@ -6,22 +6,23 @@
 #include "lexer/Token.h"
 
 class Lexer {
-    public:
-        explicit Lexer(const std::string& source);
-        std::vector<Token> tokenize();
-    private:
-        std::string source;
-        size_t position = 0;
-        size_t line = 1;
-        size_t column = 1;
+ public:
+  explicit Lexer(const std::string& source);
+  std::vector<Token> tokenize();
 
-        char peek();
-        char advance();
-        void skipWhitespace();
-        Token readInstruction();
-        Token readRegister();
-        Token readImmediate();
-        Token readPunctuation();
+ private:
+  std::string source;
+  size_t position = 0;
+  size_t line = 1;
+  size_t column = 1;
+
+  char peek();
+  char advance();
+  void skipWhitespace();
+  Token readInstruction();
+  Token readRegister();
+  Token readImmediate();
+  Token readPunctuation();
 };
 
 #endif // LEXER_H
