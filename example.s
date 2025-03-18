bc95c89bb59bb5057185c7e312e7e37d89d10f54
hirwa <hirwanshutiflx@gmail.com>
2025-03-18 23:26:03 +0530

    add tests for parser and llvm IR gen

--- example.s
+++ example.s
@@ -1,18 +1,11 @@
-; Hello World Program - asmtutor.com
-; Compile with: nasm -f elf helloworld.asm
-; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 helloworld.o -o helloworld
-; Run with: ./helloworld
-
-SECTION .data
-msg     db      'Hello World!', 0Ah     ; assign msg variable with your message string
-
+.section .data
+msg db 'Hello World!', 0Ah
 SECTION .text
-global  _start
-
-_start: 
-
-    mov     edx, 13         ; number of bytes to write - one for each letter plus 0Ah (line feed character)
-    lea     ecx, [msg]      ; load the effective address of our message string into ecx
-    mov     ebx, 1          ; write to the STDOUT file
-    mov     eax, 4          ; invoke SYS_WRITE (kernel opcode 4)
-    int     80h
+ global  _start
+_start:
+ mov edx, 13
+lea ecx, [msg]
+mov ebx, 1
+mov eax, 4
+int 80h
+end
